5d9c913cdf73cecc47d4c532430d3d4cdf76bc56
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-24 15:31:21 -0700

    react-router-dom with typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.scss";
 import Signup from "./components/authentication/Signup/Signup";
 import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Signup />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
