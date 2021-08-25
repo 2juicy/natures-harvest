2bb4184c81209713822ef8866caf4ea459a79cbd
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-25 10:46:06 -0700

    fixed some console errors

--- src/components/dashboard/Dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard/Dashboard.tsx
@@ -1,7 +1,13 @@
+import React, { useState } from "react";
+import Alert from "../../authentication/Alert/Alert";
+
 export default function Dashboard() {
+  const [error, setError] = useState("");
+
   return (
     <div>
-      <h1>This component is empty for now</h1>
+      <h1 className="title">This component is empty for now</h1>
+      {error && <Alert message={error} setError={setError} />}
     </div>
   );
 }
