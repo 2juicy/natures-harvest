13aa1ea27c0ed2966b358c158283598a895d5f44
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-25 08:56:58 -0700

    types for AuthContext complete

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useContext, useState } from "react";
+import firebase from "firebase/app";
 import { auth } from "../firebase";
 
-const AuthContext = React.createContext<any>(null);
+interface AuthContext {
+  currentUser: firebase.User | null;
+  signup: (
+    email: string,
+    password: string
+  ) => Promise<firebase.auth.UserCredential>;
+  signin: (
+    email: string,
+    password: string
+  ) => Promise<firebase.auth.UserCredential>;
+}
+
+const AuthContext = React.createContext<AuthContext | null>(null);
 
 export function useAuth() {
   return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
   const [loading, setLoading] = useState(true);
 
   function signup(email: string, password: string) {
@@ -20,7 +33,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user: any) => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
       setLoading(false);
       setCurrentUser(user);
     });
