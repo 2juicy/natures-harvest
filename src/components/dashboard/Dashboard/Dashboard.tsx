c412dc2afb950305df963d9442c1927bac038893
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-26 10:14:00 -0700

    moved logout/update functionality from Dashboard to Navbar

--- src/components/dashboard/Dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard/Dashboard.tsx
@@ -1,42 +1,19 @@
 import "./Dashboard.scss";
-import { useState } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
-import Alert from "../../authentication/Alert/Alert";
 import FlexContainer from "../../authentication/FlexContainer/FlexContainer";
-import { Link, useHistory } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 export default function Dashboard() {
-  const [error, setError] = useState("");
-  const { currentUser, signout } = useAuth();
-  const history = useHistory();
-
-  async function handleSignout() {
-    setError("");
-    try {
-      await signout();
-      history.replace("/signin");
-    } catch {
-      setError("Failed to logout");
-    }
-  }
+  const { currentUser } = useAuth();
 
   return (
     <FlexContainer>
       <Navbar />
-
       <div className="dashboard container">
         <h1 className="title">Dashboard</h1>
+        <h4>Welcome to Nature's Harvest!</h4>
         <p>Email: {currentUser?.email}</p>
-        {error && (
-          <Alert variant="alert" message={error} setMessage={setError} />
-        )}
-
-        <button className="logout" onClick={handleSignout}>
-          Log Out
-        </button>
       </div>
-      <Link to="/update-profile">Update Profile</Link>
     </FlexContainer>
   );
 }
