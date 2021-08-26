7601b5acd9b2e74d957445674972b99d8c2b215d
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-26 11:16:36 -0700

    Modal progress

--- src/components/dashboard/Modal/Modal.tsx
+++ src/components/dashboard/Modal/Modal.tsx
@@ -3,10 +3,22 @@ import "./Modal.scss";
 export default function Modal({ close }: { close: () => void }) {
   return (
     <div className="modal">
-      <h4>This is a test modal</h4>
-      <div className="modal-button-group">
-        <button onClick={close}>Cancel</button>
-        <button onClick={close}>Add</button>
+      <div>
+        <h2 className="title">Add a new plant</h2>
+        <div className="input-box">
+          <span className="label">Name</span>
+          <input placeholder="Enter plant name" type="text" required />
+        </div>
+        <div className="input-box">
+          <span className="label">Type</span>
+          <input placeholder="Enter plant type" type="text" />
+        </div>
+        <div className="modal-button-group">
+          <button className="cancel" onClick={close}>
+            Cancel
+          </button>
+          <button onClick={close}>Add</button>
+        </div>
       </div>
     </div>
   );
