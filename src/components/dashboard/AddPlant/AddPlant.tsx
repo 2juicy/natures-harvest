41c850af78548e8e4e03fef88c9086e78a435d9f
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-26 12:06:42 -0700

    Connected to firestore database
    Some warnings have been fixed in the console

--- src/components/dashboard/AddPlant/AddPlant.tsx
+++ src/components/dashboard/AddPlant/AddPlant.tsx
@@ -1,16 +1,52 @@
-import { useState } from "react";
-import Modal from "../Modal/Modal";
+import React, { useState } from "react";
 import "./AddPlant.scss";
+import { database } from "../../../firebase";
+import Modal from "../Modal/Modal";
 
 export default function AddPlant() {
   const [modal, setModal] = useState(false);
+  const [plant, setPlant] = useState({
+    name: "",
+    type: "",
+  });
+
+  function handleForm(e: React.ChangeEvent<HTMLInputElement>) {
+    setPlant({
+      ...plant,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleClose() {
+    setPlant({
+      name: "",
+      type: "",
+    });
+    setModal(false);
+  }
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    database.plants.add(plant);
+    setPlant({
+      name: "",
+      type: "",
+    });
+    setModal(false);
+  }
 
   return (
     <>
       <button className="add-plant" onClick={() => setModal(true)}>
         Add Plant
       </button>
-      {modal && <Modal close={() => setModal(false)} />}
+      {modal && (
+        <Modal
+          close={handleClose}
+          handleForm={handleForm}
+          handleSubmit={handleSubmit}
+        />
+      )}
     </>
   );
 }
