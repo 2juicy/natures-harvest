54eec90be2df46644f99a1067021e8d403237f58
JohnnyC <j0hnnywchiang@gmail.com>
2021-08-28 08:32:19 -0700

    RangeSlider component created

--- src/components/dashboard/Dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import "./Dashboard.scss";
 import { useAuth } from "../../../contexts/AuthContext";
 import Navbar from "../Navbar/Navbar";
+import RangeSlider from "../RangeSlider/RangeSlider";
 
 export default function Dashboard() {
   const { currentUser } = useAuth();
@@ -11,6 +12,8 @@ export default function Dashboard() {
       <h1 className="title">{currentUser?.email}'s dashboard</h1>
       <div className="dashboard container">
         <h4>This dashboard is not complete</h4>
+
+        <RangeSlider />
       </div>
     </div>
   );
