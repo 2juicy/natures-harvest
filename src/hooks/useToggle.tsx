c58103b5098df8c6641130aee53da34ce18f681c
JohnnyC <j0hnnywchiang@gmail.com>
2021-09-15 08:16:42 -0700

    useCallback for memoization

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export type Toggle = (arg0: boolean) => void;
 
 export default function useToggle(initialValue: boolean): [boolean, Toggle] {
   const [value, setValue] = useState(initialValue);
 
-  function toggleValue(value: boolean) {
-    setValue(currentValue =>
-      typeof value === "boolean" ? value : !currentValue
-    );
-  }
+  const toggleValue = useCallback(
+    (value: boolean) =>
+      setValue(currentValue =>
+        typeof value === "boolean" ? value : !currentValue
+      ),
+    [setValue]
+  );
 
   return [value, toggleValue];
 }
